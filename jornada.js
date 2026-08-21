b7ab0459c63acb17a8557e3c63d3e45b532fc0e1
capitan-26 <alay93.r@gmail.com>
2026-08-21 12:01:44 -0600

    Add files via upload

---
+++ jornada.js
@@ -0,0 +1,70 @@
+window.CAPITAN_CONFIG = {
+  "brand": {
+    "title": "QUINIELA CAPITÁN",
+    "tagline": "TU PASIÓN. TU INSTINTO. TU QUINIELA.",
+    "logo": "assets/capitan_logo.png"
+  },
+  "jornada": "JORNADA 2",
+  "liga": "LIGA MX C22",
+  "fecha": "14 - 16 DE ENERO",
+  "cierre": "JUEVES 10:00 PM",
+  "precioBase": 30,
+  "organizerWhatsApp": "5530014079",
+  "webAppUrl": "",
+  "partidos": [
+    {
+      "local": "Necaxa",
+      "visitante": "Monterrey",
+      "logoLocal": "assets/team_1_local.png",
+      "logoVisitante": "assets/team_1_visitante.png"
+    },
+    {
+      "local": "Atlas",
+      "visitante": "San Luis",
+      "logoLocal": "assets/team_2_local.png",
+      "logoVisitante": "assets/team_2_visitante.png"
+    },
+    {
+      "local": "Tigres",
+      "visitante": "Puebla",
+      "logoLocal": "assets/team_3_local.png",
+      "logoVisitante": "assets/team_3_visitante.png"
+    },
+    {
+      "local": "Cruz Azul",
+      "visitante": "Juárez",
+      "logoLocal": "assets/team_4_local.png",
+      "logoVisitante": "assets/team_4_visitante.png"
+    },
+    {
+      "local": "Tijuana",
+      "visitante": "León",
+      "logoLocal": "assets/team_5_local.png",
+      "logoVisitante": "assets/team_5_visitante.png"
+    },
+    {
+      "local": "Toluca",
+      "visitante": "Santos",
+      "logoLocal": "assets/team_6_local.png",
+      "logoVisitante": "assets/team_6_visitante.png"
+    },
+    {
+      "local": "Pachuca",
+      "visitante": "Guadalajara",
+      "logoLocal": "assets/team_7_local.png",
+      "logoVisitante": "assets/team_7_visitante.png"
+    },
+    {
+      "local": "Man City",
+      "visitante": "Chelsea",
+      "logoLocal": "assets/team_8_local.png",
+      "logoVisitante": "assets/team_8_visitante.png"
+    },
+    {
+      "local": "Tottenham",
+      "visitante": "Arsenal",
+      "logoLocal": "assets/team_9_local.png",
+      "logoVisitante": "assets/team_9_visitante.png"
+    }
+  ]
+};
