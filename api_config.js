8a227a86a46efec017a5cbbb8a77496b7da12f0a
capitan-26 <alay93.r@gmail.com>
2026-08-22 02:32:32 -0600

    api_config.js

--- api_config.js
+++ api_config.js
@@ -1,2 +1,4 @@
-window.CAPITAN_API_URL =
-  'https://script.google.com/macros/s/AKfycby7cAlMK4G0GqwGA5HzyDyDTevt42adY-I7cfYFnLE94P1qZBu2ocbBwUZ7fSRGKaka4Q/exec';
+window.CAPITAN_CONFIG = window.CAPITAN_CONFIG || {};
+
+window.CAPITAN_CONFIG.webAppUrl =
+'https://script.google.com/macros/s/AKfycby7cAlMK4G0GqwGA5HzyDyDTevt42adY-I7cfYFnLE94P1qZBu2ocbBwUZ7fSRGKaka4Q/exec';
